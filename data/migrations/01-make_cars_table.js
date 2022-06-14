b30a171a904582d90f26438cb2274545fc8a645a
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-14 15:37:35 -0500

    migrations up function complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,23 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
-};
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id')
+
+    tbl.string('vin', 13).notNullable().unique()
+
+    tbl.sting('make', 128).notNullable()
+   
+    tbl.sting('model', 128).notNullable()
+   
+    tbl.numeric('mileage').unsigned().notNullable()
+
+    tbl.sting('title', 128)
+   
+    tbl.sting('transmission', 128)
+
+  })
+}
+
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+
 };
