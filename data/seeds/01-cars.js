2e6c575f0f0a43f1ae7bc2a3140e94f7547c1a03
Adam Smith <adamsmith@Adams-MacBook-Air.local>
2022-06-14 15:28:12 -0500

    cars seed fleshed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,37 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: "toyota",
+        model: 'hylux',
+        milage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111111',
+        make: "toyota",
+        model: 'prius',
+        milage: 2150,
+        title: 'clean',
+    },
+    {
+        vin: '1111111111111',
+        make: "subaru",
+        model: 'outback',
+        milage: 21000,
+    },
+]
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate() 
+    await knex('cars').insert(cars)
+}
+
+// this is the exact same as the function above
+// exports.seed = function (knex) {
+//     return knex('cars')
+//         .truncate().then(() => {
+//             return knex('cars').insert(cars)
+//         })
+// }
